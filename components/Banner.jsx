9c6af0af4acd36e84c0786725c021b295f881cf4
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-02-04 03:30:30 +0600

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -15,21 +15,21 @@ const Banner = () => {
           alt="/"
         />
         <div className="absolute top-[50%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 ">
-          <h2 className="py-2 text-center text-6xl font-extrabold leading-10 tracking-normal">
+          <h2 className="pt-10 text-center text-6xl font-extrabold leading-10 tracking-wider">
             Best Place for Unfinished Games
           </h2>
-          <p className="text-center py-10">
+          <p className="text-center pt-10">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
             corporis in error?
           </p>
-          <div className="text-center px-24 md:px-96 grid md:grid-cols-2  gap-4">
+          <div className="text-center pt-14 px-24 md:px-96 grid md:grid-cols-2  gap-4">
             
-            <button className="relative text-white bg-transparent  border-2 border-cyan-700 hover:border-none focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2
-             before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-cyan-700 before:transition-transform before:duration-300 before:rounded-full hover:text-white before:hover:scale-x-100">
+            <button className="relative text-white bg-transparent  border-2 border-cyan-700 hover:border-none focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-3 text-center mr-2 mb-2
+             before:absolute before:rounded-full  before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-cyan-700 before:transition-transform before:duration-700 hover:text-white before:hover:scale-x-100">
                  Sign Up
             </button>
-            <button className="relative text-white bg-transparent  border-2 border-cyan-700 hover:border-none focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2
-             before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-cyan-700 before:transition-transform before:duration-300 before:rounded-full hover:text-white before:hover:scale-x-100">
+            <button className="relative text-white bg-transparent  border-2 border-cyan-700 hover:border-none focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-3 text-center mr-2 mb-2
+             before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-cyan-700 before:transition-transform before:duration-700 before:rounded-full hover:text-white before:hover:scale-x-100">
                  Log In
             </button>
             
